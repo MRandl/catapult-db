7aacb708fbbe24a511a8df7c2c047b9acd40d3b4
Mathis Randl <mathis.randl@epfl.ch>
2025-10-15 09:43:14 +0200

    getters for stats

--- src/statistics/stats.rs
+++ src/statistics/stats.rs
@@ -4,6 +4,13 @@ pub struct Stats {
 }
 
 impl Stats {
+    pub fn new() -> Self {
+        Stats {
+            beam_calls: 0,
+            nodes_queried: 0,
+        }
+    }
+
     /// Record into the statistics object that a new beam search call has been performed
     pub fn bump_beam_calls(&mut self) {
         self.beam_calls += 1
@@ -11,8 +18,16 @@ impl Stats {
 
     /// Record into the statistics object that a bunch of new edges were taken into consideration
     /// in the graph exploration phase
-    pub fn add_node_count(&mut self, node_amount: usize) {
+    pub fn bump_edges(&mut self, node_amount: usize) {
         self.nodes_queried += node_amount
+    }
+
+    pub fn get_beam_calls(&self) -> usize {
+        self.beam_calls
+    }
+
+    pub fn get_nodes_queried(&self) -> usize {
+        self.nodes_queried
     }
 
     /// Write all contents of the stats object to stdout. Will dump to the path at some point
